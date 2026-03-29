30a84396018237408aca997f05b2012de9558b33
aswin1015 <93429162+aswin1015@users.noreply.github.com>
2026-03-29 13:59:48 +0530

    Delete week-1 directory

--- week-1/Hackathon/problem_9/decrypt.py
+++
@@ -1,8 +0,0 @@
-from cryptography.fernet import Fernet
-
-key = open("secret.key", "rb").read()
-f = Fernet(key)
-
-encrypted = open("input.txt.encrypted", "rb").read()
-decrypted = f.decrypt(encrypted)
-open("input.txt.decrypted", "wb").write(decrypted)
--- week-1/Microservices App/user-service/src/main/java/com/ust/userservice/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ust.userservice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.ust.userservice.entity.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
